1876c3cdb6086b1b4e0209c4ca34c0dbcf6f9bd3
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-05-27 17:23:58 -0400

    First commit

---
+++ DirectX11Starter/Main.cpp
@@ -0,0 +1,38 @@
+#include "Main.h"
+#include <Windows.h>
+#include <crtdbg.h>
+
+
+
+Main::Main()
+{
+}
+
+
+Main::~Main()
+{
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance,
+					HINSTANCE hPrevInstance,
+					LPSTR lpCmdLine,
+					int nCmdShow)
+{
+
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	{
+		char currentDir[1024] = {};
+		GetModuleFileName(0, currentDir, 1024);
+		char*lastSlash = strrchr(currentDir, '\\');
+		if (lastSlash)
+		{
+			*lastSlash = 0;
+			SetCurrentDirectory(currentDir);
+		}
+	}
+
+	
+}
