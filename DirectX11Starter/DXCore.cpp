85c0a5483ddd240015015a64e9e32e9c69db7031
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-05-28 23:02:09 -0400

    progress

--- DirectX11Starter/DXCore.cpp
+++ DirectX11Starter/DXCore.cpp
@@ -16,12 +16,46 @@ unsigned int windowWidth,
 unsigned int windowHeight,
 bool debugTitleBarStats)
 {
+	DXCoreInstance = this;
+
+	// Save params
+	this->hInstance = hInstance;
+	this->titleBartext = titleBarText;
+	this->width = windowWidth;
+	this->height = windowHeight;
+	this->titleBarStats = debugTitleBarStats;
+
+	// Initialize fields
+	fpsFrameCount = 0;
+	fpsTimeElapsed = 0.0f;
+
+	device = 0;
+	context = 0;
+	swapChain = 0;
+	backBufferRTV = 0;
+	depthStencilView = 0;
+
+	// Query performance counter for accurate timing information
+	__int64 perfFreq;
+	QueryPerformanceFrequency((LARGE_INTEGER*)&perfFreq);
+	perfCounterSeconds = 1.0 / (double)perfFreq;
 }
 
 
 DXCore::~DXCore()
 {
 	//release stuff.. but what?
+	if (depthStencilView) { depthStencilView->Release(); }
+	if (backBufferRTV) { backBufferRTV->Release(); }
+
+	if (swapChain) { swapChain->Release(); }
+	if (context) { context->Release(); }
+	if (device) { device->Release(); }
+}
+
+LRESULT DXCore::ProcessMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	return LRESULT();
 }
 
 HRESULT DXCore::InitWndow()
@@ -37,5 +71,44 @@ HRESULT DXCore::InitWndow()
 	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
 	wndClass.lpszMenuName = NULL;
 	wndClass.lpszClassName = "Direct3DWindowClass";
+
+	if (!RegisterClass(&wndClass))
+	{
+		DWORD error = GetLastError();
+		if (error != ERROR_CLASS_ALREADY_EXISTS)
+			return HRESULT_FROM_WIN32(error);
+	}
+	RECT clientRect;
+	SetRect(&clientRect, 0, 0, width, height);
+	AdjustWindowRect(&clientRect, WS_OVERLAPPEDWINDOW, false);
+
+	RECT desktopRect;
+	GetClientRect(GetDesktopWindow(), &desktopRect);
+	int centeredX = (desktopRect.right / 2) - (clientRect.right / 2);
+	int ceneteredY = (desktopRect.bottom / 2) - (clientRect.bottom / 2);
+
+	hWnd = CreateWindow(
+		wndClass.lpszClassName,
+		titleBartext.c_str(),
+		WS_OVERLAPPEDWINDOW,
+		centeredX,
+		ceneteredY,
+		clientRect.right - clientRect.left,
+		clientRect.bottom - clientRect.top,
+		0,
+		0,
+		hInstance,
+		0);
+
+	if (hWnd == NULL)
+	{
+		DWORD error = GetLastError();
+		return HRESULT_FROM_WIN32(error);
+	}
+
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	return S_OK;
 }
 
--- DirectX11Starter/DXCore.h
+++ DirectX11Starter/DXCore.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Windows.h>
+#include <WindowsX.h>
 #include <d3d11.h>
 #include <string>
 #pragma comment(lib, "d3d11.lib")
@@ -16,14 +17,7 @@ public:
 	~DXCore();
 
 	static DXCore* DXCoreInstance;
-	static LRESULT CALLBACK WindowProc(
-		HWND hWnd,
-		UINT uMsg,
-		WPARAM wParam,
-		LPARAM lParam);
-
-protected:
-	HINSTANCE	hInstance;
+	static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 	//WindowProc sends it to ProcessMessage if something needs to be done
 	LRESULT ProcessMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -31,8 +25,55 @@ protected:
 	HRESULT InitWndow(); //before we can initialize DirectX 
 	HRESULT InitDirectX();
 	HRESULT Run();
-	void Quit();
-	virtual void OnResize();
+	/*void Quit();
+	virtual void OnResize();*/
+
+	virtual void Init() = 0;
+	virtual void Update(float deltaTime, float totalTime) = 0;
+	virtual void Draw(float deltaTime, float totalTime) = 0;
+
+	virtual void OnMouseDown(WPARAM buttonState, int x, int y) { }
+	virtual void OnMouseUp(WPARAM buttonState, int x, int y) { }
+	virtual void OnMouseMove(WPARAM buttonState, int x, int y) { }
+	virtual void OnMouseWheel(float wheelDelta, int x, int y) { }
+
+
+protected:
+	HINSTANCE	hInstance;
+	HWND hWnd;
+	std::string titleBartext;
+	bool titleBarStats;
+
+	unsigned int width;
+	unsigned int height;
+
+	D3D_FEATURE_LEVEL		dxFeatureLevel;
+	IDXGISwapChain*			swapChain;
+	ID3D11Device*			device;
+	ID3D11DeviceContext*	context;
+
+	ID3D11RenderTargetView* backBufferRTV;
+	ID3D11DepthStencilView* depthStencilView;
+
+	void CreateConsoleWindow(int bufferLines, int bufferColumns, int windowLines, int windowColumns);
+
+
+private:
+	// Timing related data
+	double perfCounterSeconds;
+	float totalTime;
+	float deltaTime;
+	__int64 startTime;
+	__int64 currentTime;
+	__int64 previousTime;
+
+	// FPS calculation
+	int fpsFrameCount;
+	float fpsTimeElapsed;
+
+	void UpdateTimer();			// Updates the timer for this frame
+	void UpdateTitleBarStats();
+	
 
 };
 
